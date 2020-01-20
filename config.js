247b67dc8140004f6200809cd1bade4fd3fbd5b5
sesamefly@163.com <sesamefly@163.com>
2020-01-20 15:08:25 +0800

    去掉支付宝支付

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   note: '升级地址选择器插件',
   subDomain: "xmp", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
   appid: "wxfd0f3b50b2bcb27e", // 您的小程序的appid，购物单功能需要使用
-  shareProfile: '去给我买瓶酱油！', // 首页转发的时候话术
+  shareProfile: '惊喜多多！实惠多多！', // 首页转发的时候话术
   kanjiaRequirePlayAd: true, // 是否必须要观看视频广告后才可以砍价
   goodsDetailSkuShowType: 0, // 0 为点击立即购买按钮后出现规格尺寸、数量的选择； 1为直接在商品详情页面显示规格尺寸、数量的选择，而不弹框
 }
